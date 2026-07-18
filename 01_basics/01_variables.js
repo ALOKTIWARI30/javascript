7d010f7f82d95c1147945018aa4db386b66fa468
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2026-07-18 07:20:55 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=144553
+let accountEmail="xyz@abc.com"
+var accountPass="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2
+
+accountEmail="hc@hc.com"
+accountPass="21212121"
+accountCity="Bengaluru"
+// console.log(accountId);
+// console.log(accountEmail)
+// console.log(accountPass)
+// console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+/*
+prefer not to use var because of block scope and fucntional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as a newer version
+//alert(3+3) //we are using node js not browser
+
+let name="alok"
+let age=18
+let isloggedon=false
+let state;//undefined i.e. variable is declared but value is not defined
+//let state=null
+
+//number
+//bigInt
+//string=>""
+//boolean=> true or false
+//null=>it is a standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined)//undefined
