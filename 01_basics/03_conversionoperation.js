2b01f17934bdf74d0c2fde75f5b3c7675e3b856c
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2026-07-20 15:13:36 +0000

    conversionofdatatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,44 @@
+// let score=33
+// console.log(typeof score)
+
+let score="33"
+// console.log(typeof score)
+let s="33abc"
+
+let valueinnumber=Number(score)
+// console.log(typeof valueinnumber)
+// console.log(s)
+p=Number(s)
+
+// console.log(typeof p)
+
+let a=null
+let b=Number(a)
+// console.log(typeof b)
+
+let c=undefined
+let d=Number(c)
+// console.log(typeof d)
+// console.log(d)
+
+/*
+"33"=>33
+"33abc"=>NaN
+true=>1
+false=>0
+*/
+
+let isloggedin=1
+let booleanisloggedin=Boolean(isloggedin)
+// console.log(booleanisloggedin)
+// console.log(typeof booleanisloggedin)
+
+//1=>true
+//0=>false
+//""=>false
+//"data"=>true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
