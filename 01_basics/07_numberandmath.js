ea1b1a09175a063736a4c0d510de2ce6f544bd5c
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2026-07-22 16:26:06 +0000

    numberandmath

---
+++ 01_basics/07_numberandmath.js
@@ -0,0 +1,29 @@
+const score=400
+console.log(score)
+const balance =new Number(100)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+const othernumber=23.8966
+// console.log(othernumber.toPrecision(3))
+const n=123.8966
+// console.log(n.toPrecision(3))
+const hundreds=1000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ********************. MATHS. *********************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(2.37));
+console.log(Math.ceil(4.8))
+console.log(Math.floor(4.8))
+console.log(Math.min(2,3,4,5,6));
+console.log(Math.max(2,3,4,5,6))
+console.log((Math.random()*10)+1)
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
