988031653b66d03bc271bb49a6801eab0b3203d5
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2026-07-25 13:44:46 +0000

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// let index=0
+// while (index<10) {
+//     console.log(index);
+//     index++
+// }
+
+// let myarr=["flash","batman","superman"]
+// let arr=0
+// while (arr<myarr.length) {
+//     console.log(myarr[arr]);
+//     arr++;
+// }
+
+let score=1
+do {
+    console.log(score)
+    score++
+} while (score<10);
