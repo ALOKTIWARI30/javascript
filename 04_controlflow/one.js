e12de26e370d8a585410b22d13ed062bddb300e0
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2026-07-25 12:49:43 +0000

    if and else condition

---
+++ 04_controlflow/one.js
@@ -0,0 +1,24 @@
+//if
+const userloggedin=true
+if(userloggedin)
+{
+
+}
+//<,>,<=,>=,==,!=,===,!==
+
+// const temperature=41
+// if(temperature<50)
+// {
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("greater than 50")
+// }
+// console.log("executed")
+
+const score=200
+if(score>100)
+{
+   var power="fly"
+    console.log(`user power:${power}`);
+}
