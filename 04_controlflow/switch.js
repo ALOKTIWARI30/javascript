d0dcace3449e760846af1730d02358beb42f69cb
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2026-07-25 13:15:24 +0000

    switch staements

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+//if break statements is not present then whenever a condition is matched all the 
+//code will be executed from that part except default
+const month=3
+switch (month) {
+    case 1:
+        console.log("A")
+        break;
+        case 2:
+        console.log("B")
+        break;
+        case 3:
+        console.log("C")
+        break;
+        case 4:
+        console.log("D")
+        break;
+
+    default:
+        console.log("default")
+        break;
+}
